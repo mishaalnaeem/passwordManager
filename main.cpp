5b6c90b6642442dc0d0b4e8f6d604bd0c07c2acb
Mishaal Naeem <mishaal.naeem@invozone.com>
2023-07-19 10:42:29 +0500

    driver logic

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "include/passwordManager.h"
+
+int main(){
+    PasswordManager* passwordManager = new PasswordManager();
+
+    //Check if app password is already set
+
+    //if not -> new user
+    // set up app password
+
+    //ask to enter password to get started
+
+    //list of passwords saved
+
+    //options: add new, edit, delete, view, new app password <switch>
+
+    //if add -> enter username, password, re-enter password
+
+    //if edit -> renter app password -> switch username, password -> if username, username input if password, 2 password enters
+
+    //if delete -> reenter app password
+
+    //if view -> input id from list
+
+    //if app password -> old password, new password, re enter new password
+}
